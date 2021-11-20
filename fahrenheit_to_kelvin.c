979b6902b0e3e8e6de9e7a5fb56eea5998f099e7
Abhishek <91943386+ABHISHEKAGARWAL1712@users.noreply.github.com>
2021-11-20 21:14:24 +0530

    Add files via upload

---
+++ fahrenheit_to_kelvin.c
@@ -0,0 +1,15 @@
+/*WAP to convert temperature from celsius to fahrenheit and kelvin*/
+#include <stdio.h>
+
+int main()
+{
+    float Celsius,Fahrenheit,Kelvin;
+printf("Enter the temperature in Celsius:- ");
+scanf("%f",&Celsius);
+F = (Celsius*9)/5+32;
+K = Celsius+273.15;
+printf("temperatur in farenheit is %f and in kelvin is %f \n",Fahrenheit,Kelvin);
+    
+
+    return 0;
+}
